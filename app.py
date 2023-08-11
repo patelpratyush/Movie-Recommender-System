278e84c76da49b3eb440c077191a9e84a5e4b313
patelpratyush <patelpratyush28@gmail.com>
2023-08-11 17:19:03 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pickle 
+import pandas as pd
+import requests
+
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=98b158889c10aa40f44a6a933974fd88&&language=en-US'.format(movie_id))
+    data = response.json()
+    return "http://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies,recommended_movies_posters
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title("Movie Recommender System")
+
+selected_movie_name = st.selectbox(
+    'How would you like to be contacted?',
+    movies['title'].values,
+)
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+
+    num_columns = 5  # Define the number of columns you want
+
+    cols = st.columns(num_columns)
+    for i in range(num_columns):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])
